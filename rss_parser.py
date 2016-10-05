f1b958e7af494032a08f646e92c53b532e9d570e
EmileSchneider <emileschneider7@gmail.com>
2016-10-05 14:47:25 +0200

    Add files via upload
    Der Update des Skripts

--- rss_parser.py
+++ rss_parser.py
@@ -1,16 +1,12 @@
 import feedparser
 
 rss_url = "http://www.spiegel.de/schlagzeilen/index.rss"
-
 feed = feedparser.parse(rss_url)
-
 item_list = []
 
 for item in feed['items']:
     item_list.append(item['title'])
 
-with open('./output.txt', mode='wt', encoding='utf-8') as file:
+with open('./output.txt', mode = 'wt', encoding = 'utf-8') as file:
     for title in item_list:
         file.write(title + '\n')
-
-
